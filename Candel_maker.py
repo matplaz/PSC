609a5673f7fcbabe3d6df21ff0b5dbf60feaad2d
AntGro <33458508+AntGro@users.noreply.github.com>
2018-01-31 20:25:28 +0100

    Candel maker
    Permet d'importer les données depuis Kraken et de créer de formater les données en chandelles

--- Candel_maker.py
+++ Candel_maker.py
@@ -4,83 +4,141 @@ import pandas as pd
 import numpy as np
 import time
 from datetime import date
+import matplotlib.pyplot as plt
 
-#pair à charger
-i=1 #choix de la paire
-pairs=['XXBTZEUR','XETHZEUR']
+#==========================================================================
+#CANDEL-MAKER --- GUIDE
+#==========================================================================
+
+#   -> définir un fichier où les données seront enregistrées (modifier "racine") (I.)
+#   -> choisir la pair de crypto-monnaie (choisir la valeur de i)  (II.)
+#   -> Téléchargement des données transactions via API (III.)
+#   -> Mettre sous forme de chandelles (IV.)
+
+
+#==========================================================================
+#   I. STOCKAGE DATA
+#==========================================================================
+
+#Dossier où enregistrer les données
+
+racine='C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/'
+
+#==========================================================================
+#   II. CHOIX PAIR CRYPTO
+#==========================================================================
+
+i=3   #choix de la paire
+pairs=['XXBTZEUR','XETHZEUR','XLTCZEUR','BCHEUR','DASHEUR']
 pair=pairs[i]
 
-#Chargement des opérations sur Kraken via l'API
-def f(j=1):
+
+#==========================================================================
+#   III. TELECHARGEMENT TRANSACTION KRAKEN API
+#==========================================================================
+
+#Fonction principale : chargement des opérations sur Kraken via l'API
+
+    #   f permet de télécharger les données depuis le début de l'historique des transactions (ou de reprendre depuis le moment où vous vous êtes arrêtés).
+    #   Les données brutes sont ajoutées une à une sur un pandasFrame  et sont mises en forme et enregistées sur un autre pandasFrame à intervelles réguliers.
+    
+    #@ PARAMETRE  j: fixe le nombre de jours de données que vous voulez télécharger entre chaque sauvegarde
+    
+    #Au cours du processus, il ne FAUT PAS fermer python si le dernier message affiché est "you cannot interrupt"... Il faut attendre l'apparition de "Now you can"
+    
+def f(j=15):
     
     #paramètres
     current=time.time()
     
-    
-    #récupération du dernier id chargé
-    file = open('C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/Data_'+pair+'/last_id_'+pair+'.txt',"r") 
-    
-    last_id= file.readlines()[0]
-    
-    file.close()
-    
     #charger données déjà existantes
-    dataset = pd.read_csv(open('C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/Data_'+pair+'/New_Data_'+pair+'.csv'), index_col=0)
+    try:
+        dataset = pd.read_csv(open(racine +'Data_'+pair+'/New_Data_'+pair+'.csv'), index_col=0)
+    except Exception:
+        dataset=pd.DataFrame()
     
-    while(int(last_id)/10**9<current):#tant que l'on n'a pas toute les données jusqu'aujourd'hui
-        #initialisation
+    #charge l'id du dernier paquet téléchargé à partir du fichier "last_id"
+    try :
+        file = open(racine + 'Data_'+pair+'/last_id_'+pair+'.txt',"r") 
+        last_id= file.readlines()[0]
+        file.close()
+    except Exception:
+        last_id='0'
+        
+    n=0
+    
+    #Chargement des données jusqu'aujourd'hui
+    while(int(last_id)/10**9<current):
         data=pd.DataFrame()
-        try:
-            while(True):#on ajoute les blocs de 1000 données un par un jusqu'à obtenir une exception
-                url='https://api.kraken.com/0/public/Trades?pair='+pair+'&since='+last_id
-                new_data=requests.get(url)
-                last_id=new_data.json()['result']['last']
-                data=data.append(new_data.json()['result']['XXBTZEUR'], ignore_index=True)
-                print(last_id)
-        except Exception: #on profite de l'exception pour enregistrer les données chargées et relancer le programme
-            print()
-            print(str(j)+" crash"+'\n')
-            data.columns=['price', 'volume', 'time', 'buy/sell', 'market/limit', 'miscellaneous']
-            dataset=dataset.append(data,ignore_index=True)
-            dataset.to_csv('C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/Data_'+pair+'/New_Data_'+pair+'.csv')
-            file = open('C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/Data_'+pair+'/last_id_'+pair+'.txt',"w")
-            file.write(last_id)
-            file.close()
-            time.sleep(j%30)
-            f(j+1)
-    
+        dif=0
+        while(dif<24*3600*10**9*j):
+            dif-=int(last_id)
+            url='https://api.kraken.com/0/public/Trades?pair='+pair+'&since='+last_id
+            (last_id,data)=get_answer(url,data)
+            print(last_id)
+            dif+=int(last_id)
+        n+=1
+        print('{} savings'.format(n))
+        print("\nyou cannot interrupt")
+        time.sleep(5)
+        data.columns=['price', 'volume', 'time', 'buy/sell', 'market/limit', 'miscellaneous']
+        dataset=dataset.append(data,ignore_index=True)
+        dataset.to_csv(racine + 'Data_'+pair+'/New_Data_'+pair+'.csv')
+        file = open(racine + 'Data_'+pair+'/last_id_'+pair+'.txt',"w")
+        file.write(last_id)
+        file.close()
+        print("now you can\n")
     print("The end")
 
 
-def transfert():#transfert des données chargées depuis un certain temps vers le fichier contenant toutes les opérations
-    
-    dataset = pd.read_csv(open('C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/Data_'+pair+'/Data_'+pair+'.csv'), index_col=0)
+# Fonction auxilliaire de f
 
-    data_add = pd.read_csv(open('C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/Data_'+pair+'/New_Data_'+pair+'.csv'), index_col=0)
+def get_answer(url,data):
+    new_data=requests.get(url)
+    try:
+        last_id=new_data.json()['result']['last']
+    except Exception:
+        return get_answer(url,data)
+    data=data.append(new_data.json()['result'][pair], ignore_index=True)
+    return (last_id,data)
+
+#===================================================================
+#   A utiliser lorsque le ficher newDataXXX/ZEUR devient trop lourd
+    #   Transfert des données de newDataXXX/ZEUR vers DataXXX/ZEUR
+
+def transfert():
     
+    try:
+        dataset = pd.read_csv(open(racine + 'Data_'+pair+'/Data_'+pair+'.csv'), index_col=0)
+    except Exception:
+        dataset=pd.DataFrame()
     
+
+    data_add = pd.read_csv(open(racine + 'Data_'+pair+'/New_Data_'+pair+'.csv'), index_col=0)
     
     dataset=dataset.append(form(data_add,dataset.shape[0]),ignore_index=True)
     
-    data_add=pd.DataFrame(columns=['price', 'volume', 'time', 'buy/sell', 'market/limit', 'miscellaneous'])
-    data_add.to_csv('C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/Data_'+pair+'/New_Data_'+pair+'.csv')
+    data_add=pd.DataFrame(columns=['price', 'volume', 'volume_EUR', 'time', 'buy/sell', 'market/limit', 'miscellaneous'])
+    data_add.to_csv(racine + 'Data_'+pair+'/New_Data_'+pair+'.csv')
     
+    dataset.to_csv(racine + 'Data_'+pair+'/Data_'+pair+'.csv')
     
-    dataset.to_csv('C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/Data_'+pair+'/Data_'+pair+'.csv')
-    
-    file = open('C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/Data_'+pair+'/last_id_'+pair+'.txt',"r") 
+    file = open(racine + 'Data_'+pair+'/last_id_'+pair+'.txt',"r") 
     last_id= file.readlines()[0]
     file.close()
     
-    file = open('C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/Data_'+pair+'/previous_last_id_'+pair+'.txt',"w")
+    file = open(racine + 'Data_'+pair+'/previous_last_id_'+pair+'.txt',"w")
     file.write(last_id)
     file.close()
 
 
-def form(dataset,decalage=0):
-    data=pd.DataFrame(columns=['time','price','volume','buy','market','candle_24h','candle_4h','candle_1h','candle_15min','candle_1min','batch_id'])
+#   Fonction auxilliaire de transfert
+
+def form(dataset,decalage=0):#met les données au format ['time','price','volume','buy','market','candle_24h','candle_4h','candle_1h','candle_15min','candle_1min','batch_id'] en vue de former les chandelles
+    data=pd.DataFrame(columns=['time','price','volume','volume_EUR','buy','market','candle_24h','candle_4h','candle_1h','candle_15min','candle_1min','batch_id'])
     
     data[['time','price','volume','buy','market']]=dataset[['time','price','volume','buy/sell','market/limit']]
+    data['volume_EUR']=data['volume']*data['price']
     data['candle_24h']=data['time'].apply(lambda x: int(x/(24*3600)))
     data['candle_4h']=data['time'].apply(lambda x: int(x/(4*3600)))
     data['candle_1h']=data['time'].apply(lambda x: int(x/(3600)))
@@ -90,28 +148,17 @@ def form(dataset,decalage=0):
     data['batch_id']=data['batch_id'].apply(lambda x: int((x+decalage)/1000))
 
     return data
-
-
-def formater():#met les données au format ['time','price','volume','buy','market','candle_24h','candle_4h','candle_1h','candle_15min','candle_1min','batch_id'] en vue de former les chandelles
-    datest = pd.read_csv(open('C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/Data_'+pair+'/Data_'+pair+'.csv'), index_col=0)
-
-    data=pd.DataFrame(columns=['time','price','volume','buy','market','candle_24h','candle_4h','candle_1h','candle_15min','candle_1min','batch_id'])
     
-    data[['time','price','volume','buy','market']]=datest[['time','price','volume','buy/sell','market/limit']]
-    data['candle_24h']=data['time'].apply(lambda x: int(x/(24*3600)))
-    data['candle_4h']=data['time'].apply(lambda x: int(x/(4*3600)))
-    data['candle_1h']=data['time'].apply(lambda x: int(x/(3600)))
-    data['candle_15min']=data['time'].apply(lambda x: int(x/(15*60)))
-    data['candle_1min']=data['time'].apply(lambda x: int(x/60))
-    data['batch_id']=data.index.values
-    data['batch_id']=data['batch_id'].apply(lambda x: int(x/1000))
     
-    data.to_csv('C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/Data_'+pair+'/Data_'+pair+'.csv')
+#==========================================================================
+#   IV. FORMATAGE -- CHANDELLES
+#==========================================================================
 
+#   Creation des chandelles (dans le cas d'une première création de chandelles)
 
+def candle(x=24):#x:sélection de la chandelle [24h,4h,1h,15min]
 
-def candel(x):#x:sélection de la chandelle [24,4,15,1]
-    dataset = pd.read_csv(open('C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/Data_'+pair+'/Data_'+pair+'.csv'), index_col=0) #import fichier csv brut
+    dataset = pd.read_csv(open(racine + 'Data_'+pair+'/Data_'+pair+'.csv'), index_col=0) #import fichier csv brut
     
     if(x==24 or x==4 or x==1):
         can='candle_'+str(x)+'h'
@@ -125,32 +172,147 @@ def candel(x):#x:sélection de la chandelle [24,4,15,1]
         can='candle_'+str(x)+'min'
         if x==15:
             ind=-3
+            m=15*60
         else:
             ind=-2
+
+    newCandle=pd.DataFrame(columns=['nb','V','V_EUR','Pmin','PQ1','Pmed','PQ3','Pmax','P_open','P_close','return']) # nb : nombre d'opération, V:volume echange en crypto , Pmin,PQ1,Pmed,PQ3,Pmax:min, premier quartile, médiane, troisième quartile, max des prix des échanges d'ordre 'market', 'return':(P_close-P_open)/P_open, P_open : premier prix de la chandelle
     
+    df=dataset.iloc[:,[1,2,3,ind]] #crée un dataFrame à partir des collonees 'volume','volume_EUR', 'price', 'candle_...'
     
-    df=dataset.iloc[:,[1,2,3,4,ind]] #crée un dataFrame à partir des collonees 'volume', 'market', 'price', 'candle_15min'
-    
-    
-    chandelles=pd.DataFrame(columns=['nb','V','Pmin','PQ1','Pmed','PQ3','Pmax','P_open','P_close','return']) # nb : nombre d'opération, V:volume echange , Pmin,PQ1,Pmed,PQ3,Pmax:min, premier quartile, médiane, troisième quartile, max des prix des échanges d'ordre 'market', 'return':(P_close-P_open)/P_open, P_open : premier prix de la journée
     
     d_group=df.groupby(can)
 
-    
     #Remplissage des colonnes
     
-    chandelles['nb']=d_group['volume'].count()
-    chandelles['V']=d_group['volume'].apply(lambda x: x.sum())
-    chandelles['Pmin']=d_group['price'].apply(lambda x: x.min())
-    chandelles['PQ1']=d_group['price'].apply(lambda x: x.quantile(0.25))
-    chandelles['Pmed']=d_group['price'].apply(lambda x: x.quantile(0.5))
-    chandelles['PQ3']=d_group['price'].apply(lambda x: x.quantile(0.75))
-    chandelles['Pmax']=d_group['price'].apply(lambda x: x.max())
-    chandelles['P_open']=d_group['price'].apply(lambda x: x.iloc[0])
-    chandelles['P_close']=d_group['price'].apply(lambda x: x.iloc[-1])
-    chandelles['return']=(chandelles['P_close']-chandelles['P_open'])/chandelles['P_open']
+    newCandle['nb']=d_group['volume'].count()
+    newCandle['V']=d_group['volume'].apply(lambda x: x.sum())
+    newCandle['V_EUR']=d_group['volume_EUR'].apply(lambda x: x.sum())
+    newCandle['Pmin']=d_group['price'].apply(lambda x: x.min())
+    newCandle['PQ1']=d_group['price'].apply(lambda x: x.quantile(0.25))
+    newCandle['Pmed']=d_group['price'].apply(lambda x: x.quantile(0.5))
+    newCandle['PQ3']=d_group['price'].apply(lambda x: x.quantile(0.75))
+    newCandle['Pmax']=d_group['price'].apply(lambda x: x.max())
+    newCandle['P_open']=d_group['price'].apply(lambda x: x.iloc[0])
+    newCandle['P_close']=d_group['price'].apply(lambda x: x.iloc[-1])
+    newCandle['return']=(newCandle['P_close']-newCandle['P_open'])/newCandle['P_open']
     
-    
+    newCandle=newCandle.drop(newCandle.index[-1]) #on supprime la dernière ligne qui est probablement incomplète
     
     #enregistrement du nouveau fichier
-    chandelles.to_csv('C:/Users/antoine/Desktop/Polytechnique/Travail/2A/PSC/Données EY/Data_'+pair+'/Chandelles_'+pair+'_'+can+'.csv')
+    newCandle.to_csv(racine + 'Data_'+pair+'/'+pair+'_'+can+'.csv')
+
+
+#   Update des chandelles (à partir d'un fichier préexistant)
+
+def completeCandle(x=24):#Complete un document contenant les chandelles déjà téléchargées. x:sélection de la chandelle [24h,4h,1h,15min] 
+    dataset = pd.read_csv(open(racine + 'Data_'+pair+'/Data_'+pair+'.csv'), index_col=0) #import fichier csv brut
+    
+    if(x==24 or x==4 or x==1):
+        can='candle_'+str(x)+'h'
+        if x==24:
+            ind=-6
+            m=24*3600
+        elif ind==4:
+            ind=-5
+            m=4*3600
+        else:
+            ind=-4
+            m=3600
+    else:
+        can='candle_'+str(x)+'min'
+        if x==15:
+            ind=-3
+            m=15*60
+        else:
+            ind=-2
+            m=60
+
+    chandelles=pd.read_csv(open(racine + 'Data_'+pair+'/'+pair+'_'+can+'.csv'), index_col=0) #import du fichier contenant les chandelles déjà enregistrées
+    df=dataset[dataset.time>m*(chandelles.index[-1]+1)-0.001].iloc[:,[1,2,3,ind]] #crée un dataFrame à partir des collonees 'volume', 'market', 'price', 'candle_...'
+    
+    newCandle=pd.DataFrame(columns=['nb','V','V_EUR','Pmin','PQ1','Pmed','PQ3','Pmax','P_open','P_close','return'])
+    
+    d_group=df.groupby(can)
+
+    #Remplissage des colonnes
+    
+    newCandle['nb']=d_group['volume'].count()
+    newCandle['V']=d_group['volume'].apply(lambda x: x.sum())
+    newCandle['V_EUR']=d_group['volume_EUR'].apply(lambda x: x.sum())
+    newCandle['Pmin']=d_group['price'].apply(lambda x: x.min())
+    newCandle['PQ1']=d_group['price'].apply(lambda x: x.quantile(0.25))
+    newCandle['Pmed']=d_group['price'].apply(lambda x: x.quantile(0.5))
+    newCandle['PQ3']=d_group['price'].apply(lambda x: x.quantile(0.75))
+    newCandle['Pmax']=d_group['price'].apply(lambda x: x.max())
+    newCandle['P_open']=d_group['price'].apply(lambda x: x.iloc[0])
+    newCandle['P_close']=d_group['price'].apply(lambda x: x.iloc[-1])
+    newCandle['return']=(newCandle['P_close']-newCandle['P_open'])/newCandle['P_open']
+    
+    newCandle=newCandle.drop(newCandle.index[-1]) #on supprime la dernière ligne qui est probablement incomplète
+    
+    chandelles=chandelles.append(newCandle,ignore_index=False)
+    #enregistrement du nouveau fichier
+    chandelles.to_csv(racine + 'Data_'+pair+'/'+pair+'_'+can+'.csv')
+
+#===================================================================
+
+#   Creation des chandelles PLEINES (même s'il n'y a pas de transaction, il y a une ligne dans le csv) (dans le cas d'une première création de chandelles)
+#   A utiliser après avoir créé les chandelles standards
+
+def filledCandles(x=24):
+    
+    if(x==24 or x==4 or x==1):
+        can='candle_'+str(x)+'h'
+        
+    else:
+        can='candle_'+str(x)+'min'
+        
+    
+    dataset = pd.read_csv(open(racine + 'Data_'+pair+'/'+pair+'_'+can+'.csv'), index_col=0)
+    
+    data=dataset.reindex(np.arange(dataset.index[0],dataset.index[-1]+1))
+    
+    for j in range(dataset.index[0],dataset.index[-1]):
+        if(np.isnan(data.loc[j][0])):
+            data.loc[j]['nb':'V']=0
+            data.loc[j]['Pmin':'P_close']=data.loc[j-1]['P_close']
+            data.loc[j]['return']=0
+    
+    data=data.drop(data.index[-1])
+    
+    data.to_csv(racine + 'Data_'+pair+'/'+pair+'_complete_'+can+'.csv')
+
+
+#   Update des chandelles PLEINES (même s'il n'y a pas de transaction, il y a une ligne dans le csv) (à partir d'un fichier préexistant)    
+#   A utiliser après avoir créé les chandelles standards
+
+def completeFilledCandles(x=24):
+    
+    if(x==24 or x==4 or x==1):
+        can='candle_'+str(x)+'h'
+        
+    else:
+        can='candle_'+str(x)+'min'
+        
+    
+    newData = pd.read_csv(open(racine + 'Data_'+pair+'/'+pair+'_'+can+'.csv'), index_col=0)
+    
+    data=pd.read_csv(open(racine + 'Data_'+pair+'/'+pair+'_complete_'+can+'.csv'), index_col=0)
+    
+    newData=newData[newData.index>data.index[-1]-1]
+    newData=newData.reindex(np.arange(data.index[-1],newData.index[-1]+1))
+    
+    for j in range(newData.index[1],newData.index[-1]):
+        if(np.isnan(newData.loc[j][0])):
+            newData.loc[j]['nb':'V']=0
+            newData.loc[j]['Pmin':'P_close']=newData.loc[j-1]['P_close']
+            newData.loc[j]['return']=0
+        
+    newData=newData.drop(newData.index[0])
+    newData=newData.drop(newData.index[-1])
+    
+    data=data.append(newData,ignore_index=False)
+    
+    data.to_csv(racine + 'Data_'+pair+'/'+pair+'_complete_'+can+'.csv')
+            
